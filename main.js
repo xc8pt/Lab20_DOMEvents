3bb6180c3d83cfb05c3b95d6d416aa0e8c7aa85e
karasidnez <karasdnez@gmail.com>
2026-03-31 12:37:00 +0300

    Step 4: modifying DOM content and styles

--- main.js
+++ main.js
@@ -6,3 +6,13 @@ const title = document.getElementById("title");
 const text = document.querySelector(".text");
 console.log(title);
 console.log(text);
+
+//step 4
+title.textContent = "DOM изменён через JS!";
+title.style.color = "blue";
+title.style.backgroundColor = "#f0f0f0";
+title.style.padding = "10px";
+
+text.style.fontSize = "18px";
+text.style.fontStyle = "italic";
+text.style.color = "darkgreen";
